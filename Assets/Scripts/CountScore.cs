70fc684accba20d3c0df0007ef0584ab2dbf200e
JanPintoStrohhaeusl <495565@student.fontys.nl>
2022-06-28 19:08:43 +0200

    weitergemacht

--- Assets/Scripts/CountScore.cs
+++ Assets/Scripts/CountScore.cs
@@ -1,25 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountScore : MonoBehaviour
 {
-    private int score = 0;
+    public Text totalClicksText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    int totalClicks;
 
     public void OnMouseUp()
     {
-        score++;
+        totalClicksText.text = totalClicksText.ToString();
     }
 }
